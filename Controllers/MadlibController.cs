6ab056a5166ccce0802dc674fd625208bb1660c9
Starrleto <ellierasuli@gmail.com>
2024-01-10 12:07:13 -0800

    kara kara kara kara

--- Controllers/MadlibController.cs
+++ Controllers/MadlibController.cs
@@ -17,9 +17,9 @@ namespace RasuliEEndpointsD2.Controllers
         }
 
         [HttpGet]
-        [Route("Play/{words1}/{words2}/{words3}/{words4}/{words5}/{words6}/{words7}/{words8}/{words9}/{words10}")] //I guess the param here has to be the same name as the param in the method
-        public string[] food (string words1, string words2, string words3, string words4, string words5, string words6, string words7, string words8, string words9, string words10){
-            return _madlibService.play(words1, words2, words3, words4, words5, words6, words7, words8, words9, words10);
+        [Route("Play/{LivingThing}/{Emotion}/{Noun}/{Name}/{Adjective}/{AnotherName}/{AnotherObject}/{NewLivingThing}/{NewNoun}/{PlaceToHide}")] //I guess the param here has to be the same name as the param in the method
+        public string[] food (string LivingThing, string Emotion, string Noun, string Name, string Adjective, string AnotherName, string AnotherObject, string NewLivingThing, string NewNoun, string PlaceToHide){
+            return _madlibService.play(LivingThing, Emotion, Noun, Name, Adjective, AnotherName, AnotherObject, NewLivingThing, NewNoun, PlaceToHide);
         }
     }
 }
